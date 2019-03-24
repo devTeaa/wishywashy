33d691375f13265b45a8eff91df3c56cb803e2b2
devtea <devtea@protonmail.com>
2019-03-24 12:57:07 +0700

    fix gitignore

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// This file is in .gitignore
+const servicesUrl = "https://wishywashy-server.herokuapp.com";
+const datetimeFormat = "YYYY-MM-DDTHH:mm:ss.SSSS[Z]";
+
+export default servicesUrl;
+export { datetimeFormat };
